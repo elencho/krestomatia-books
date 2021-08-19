8bf23cc3a1aeb7f6f728e9dda312b2a2874e5c12
elencho <e.bochoradze@gmail.com>
2021-08-20 00:11:02 +0400

    feat: onlyMain feature

--- src/screens/BookSum.tsx
+++ src/screens/BookSum.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {View, Text, Image, TouchableOpacity} from 'react-native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import {Names} from '../../enums';
+import {Names, BookType} from '../../enums';
+import {Books} from '../../data';
 
 import st from '../components/style';
 interface Props {
@@ -21,19 +22,52 @@ const BookSum: React.FC<Props> = props => {
   } else {
     imageUrl = require('../../assets/book-screens/shushaniki.jpg');
   }
+  let onlyMain: string;
+  if (name === Names.Grigol) {
+    onlyMain = Books[2].onlyMain;
+  } else if (name === Names.Abo) {
+    onlyMain = Books[1].onlyMain;
+  } else {
+    onlyMain = Books[0].onlyMain;
+  }
+
   return (
     <View style={st.bookSum}>
       <Image source={imageUrl} style={st.bookImg} resizeMode={'contain'} />
       <TouchableOpacity
-        onPress={() => navigation.navigate('Read', {title: title, name: name})}
+        onPress={() =>
+          navigation.navigate('Read', {
+            title: title,
+            name: name,
+            type: BookType.Full,
+          })
+        }
         activeOpacity={0.7}
         style={st.buttonSmall}>
         <Text style={st.title}>სრულად</Text>
       </TouchableOpacity>
-      <TouchableOpacity activeOpacity={0.7} style={st.buttonSmall}>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('Read', {
+            title: title,
+            name: name,
+            type: BookType.Main,
+          })
+        }
+        activeOpacity={0.7}
+        style={st.buttonSmall}>
         <Text style={st.title}>შინაარსი</Text>
       </TouchableOpacity>
-      <TouchableOpacity activeOpacity={0.7} style={st.buttonSmall}>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('TextScreen', {
+            title: title,
+            name: name,
+            text: onlyMain
+          })
+        }
+        activeOpacity={0.7}
+        style={st.buttonSmall}>
         <Text style={st.title}>მოკლე შინაარსი</Text>
       </TouchableOpacity>
     </View>
